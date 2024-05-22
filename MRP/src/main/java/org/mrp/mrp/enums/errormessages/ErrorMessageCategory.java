9f8489c17ab7b751755703f93e56384001a2e736
Rytis <138163190+RytisGL@users.noreply.github.com>
2024-05-23 01:35:37 +0300

    Multi languages, multi environment setup, validation error and exception message multi language config.

---
+++ MRP/src/main/java/org/mrp/mrp/enums/errormessages/ErrorMessageCategory.java
@@ -0,0 +1,11 @@
+package org.mrp.mrp.enums.errormessages;
+
+public enum ErrorMessageCategory {
+    HTTP_MESSAGE_NOT_READABLE,
+    TYPE_MISMATCH,
+    ENTITY_NOT_FOUND,
+    METHOD_ARGUMENT_NOT_VALID,
+    SQL_EXCEPTION,
+    NUMBER_FORMAT_EXCEPTION,
+    UNSPECIFIED_EXCEPTION
+}
